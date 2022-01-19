7fac80f35bdbfd6605c90f081be40c17a0cad228
Antoine Rimonteil <antoinerimonteil@hotmail.com>
2022-01-19 17:22:55 +0100

    Fixed a bug

--- scripts.js
+++ scripts.js
@@ -1,26 +1,15 @@
-// Target the modal ...
-var modal = document.getElementById("modal-window");
-// ... the image ...
+var modal = document.getElementById("myModal");
+var btn = document.getElementsByClassName("myBtn")[0];
+var span = document.getElementById("close");
 
-// getElementsByClassName return a collection of items, starting at 0.
-// I need the modal to open if i click on any image.
-
-
-var image = document.getElementsByClassName("image-target")[6];
-// and the "close" button.
-var close_modal = document.getElementsByClassName("close-modal")[0];
-
-// Open the modal window when user click on image
-image.onclick = function() {
+btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// Close the modal window when user click on the "close" icon
-close_modal.onclick = function() {
+span.onclick = function() {
     modal.style.display = "none";
 }
 
-// Close the modal window when user click outside of the modal window
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
