8016ee3a646122141bd82da6cf6db79aa6bee530
Antoine Rimonteil <antoinerimonteil@hotmail.com>
2022-01-26 15:43:55 +0100

    script to fetch url is done

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,18 +1,46 @@
-// API URL as constants
+// API url
 const titleAPI_url = 'http://localhost:8000/api/v1/titles/';
 
-// First, let's generate the urls
+// API navigation
+const ask = '?';
+const sortByScoreAsc = 'sort_by=-imdb_score&';
+const sortByGenreCrime = 'genre=crime&';
+const sortByGenreHorror = 'genre=horror&';
+const sortByGenreWestern = 'genre=western&';
 
+// Lists to store urls of pages
+var topMoviesOverall = [];
+var topMoviesCrime = [];
+var topMoviesHorror = [];
+var topMoviesWestern = [];
+
+// Push 10 movie's complete page into a list
+async function urlGrab(url, list) {
+    var response = await fetch(url);
+    var data = await response.json();
+
+    while (list.length < 10) {
+        var resultsPerPage = 5;
+
+        while (resultsPerPage > 0) {
+            response = await fetch(data.next);
+            data = await response.json();
+
+            for (let x of data.results) {
+                list.push(x.url);
+                resultsPerPage --;
+            }
+        }
+    } 
+}
+
+urlGrab(titleAPI_url, topMoviesOverall)
 
 /////////////////////////////////////////////////// TEST: START
 
 async function getApi(url) {
     const response = await fetch(url);
-    console.log(response);
-
     var data = await response.json();
-    console.log(data);
-    
     show(data);
 }
 
